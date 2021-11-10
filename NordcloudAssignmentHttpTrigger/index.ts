fbe32f28c511b6cf90acc410372be8a9b84d7105
Michal Urban <m.urban@avanade.com>
2021-11-10 16:41:50 +0100

    Adding Azure Function

---
+++ NordcloudAssignmentHttpTrigger/index.ts
@@ -0,0 +1,17 @@
+import { Program } from "./program";
+
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const program = new Program()
+    const result = program.getResult()
+
+    context.log(result);
+
+    context.res = {
+        body: result
+    };
+
+};
+
+export default httpTrigger;
---
+++ NordcloudAssignmentHttpTrigger/program.ts
@@ -0,0 +1,22 @@
+import { TestCaseSet } from "./testCaseSet";
+
+export class Program {
+	getResult(): string {
+		const testCaseSet = new TestCaseSet()
+		let result = ''
+
+		for (const deviceTestCase of testCaseSet.devices) {
+			const linkStationWithMostPower = deviceTestCase.getLinkStationWithMostPower(testCaseSet.linkStations)
+
+			const isLinkStationReachableFromDevice = linkStationWithMostPower !== null
+
+			if (isLinkStationReachableFromDevice) {
+				result = `Best link station for point ${deviceTestCase.x},${deviceTestCase.y} is ${linkStationWithMostPower.x},${linkStationWithMostPower.y} with power ${linkStationWithMostPower.getPower(deviceTestCase)}`
+			} else {
+				result = `No link station within reach for point ${deviceTestCase.x},${deviceTestCase.y}`
+			}
+		}
+
+		return result;
+	}
+}
